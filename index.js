9d32adf17685892ad93c5e4c95648622aa17769e
Josiah Witt <josiah@witt.life>
2017-12-07 21:19:40 -0500

    Add a tags index

--- index.js
+++ index.js
@@ -5,11 +5,79 @@ exports.name = 'about'
 exports.version = require('./package.json').version
 exports.manifest = {
   stream: 'source',
-  get: 'async'
+  get: 'async',
+  tagsStream: 'source',
+  tagsGet: 'async'
 }
 
 exports.init = function (ssb, config) {
-  return ssb._flumeUse('about', FlumeReduce(1, reduce, map))
+  var about = ssb._flumeUse('about', FlumeReduce(1, reduce, map))
+  var tags = ssb._flumeUse('tags', FlumeReduce(1, tagsReduce, tagsMap))
+
+  return {
+    stream: about.stream,
+    get: about.get,
+    tagsStream: tags.stream,
+    tagsGet: tags.get 
+  }
+}
+
+function tagsReduce (result, item) {
+  if (!result) result = {}
+  if (item) {
+    for (var author in item) {
+      var valuesForAuthor = result[author] = result[author] || {}
+      var tagsForAuthor = valuesForAuthor['tags'] = valuesForAuthor['tags'] || {}
+      var msgsForAuthor = valuesForAuthor['msgs'] = valuesForAuthor['msgs'] || {}
+      for (var tag in item[author]['tags']) {
+        var msgsForTag = tagsForAuthor[tag] = tagsForAuthor[tag] || {}
+        for (var msg in item[author]['tags'][tag]) {
+          var timestamp = item[author]['tags'][tag][msg]
+          if (!msgsForTag[msg] || timestamp > msgsForTag[msg]) {
+            msgsForTag[msg] = timestamp
+          }
+        }
+      }
+      for (var msg in item[author]['msgs']) {
+        var tagsForMsg = msgsForAuthor[msg] = msgsForAuthor[msg] || {}
+        var nextTags = item[author]['msgs'][msg]
+        var timestamp = Object.values(nextTags)[0]
+        for (var tag in tagsForMsg) {
+          if (timestamp && !(tag in nextTags) && timestamp > tagsForMsg[tag]) {
+            tagsForAuthor[tag][msg] = null
+          }
+        }
+        msgsForAuthor[msg] = nextTags
+      }
+    }
+  }
+  return result
+}
+
+function tagsMap (msg) {
+  if (msg.value.content && msg.value.content.type === 'about' && msg.value.content.tags && ref.isLink(msg.value.content.about)) {
+    var author = msg.value.author
+    var target = msg.value.content.about
+    var tags = {}
+    var msgs = {
+      [target]: {}
+    }
+
+    for (var index in msg.value.content.tags) {
+      var tag = msg.value.content.tags[index]
+      tags[tag] = {
+        [target]: msg.value.timestamp
+      }
+      msgs[target][tag] = msg.value.timestamp
+    }
+
+    return {
+      [author]: {
+        'tags': tags,
+        'msgs': msgs
+      }
+    }
+  }
 }
 
 function reduce (result, item) {
